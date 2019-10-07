e3cea25e560bdc98627a309444cd2209261f7b08
100176905 <jacobacosta32@yahoo.com>
2019-10-07 15:28:13 -0500

    Part 2 of today added

--- WeekOf10-7.py
+++ WeekOf10-7.py
@@ -8,3 +8,16 @@ y = "Those who know %s and those who %s" % (binary, doNot)
 print(x)
 print(y)
 
+print("i said: %r.:" % x)
+print("I also said: '%s'." % y)
+
+hilarious = True
+jokeEvaluation = "Isn't that joke so funny?! %r"
+
+Print(jokeEvaluation % hilarious)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w+e)
+
+# More printing fun
