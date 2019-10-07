9cedea5056666b1e951fb463d0d57bfadcfa06dd
100176905 <jacobacosta32@yahoo.com>
2019-10-07 15:19:11 -0500

    Part 1 of today added

---
+++ WeekOf10-7.py
@@ -0,0 +1,10 @@
+# More strings and text
+
+x = "there are %d types of people." % 10
+binary = "binary"
+doNot = "don't"
+y = "Those who know %s and those who %s" % (binary, doNot)
+
+print(x)
+print(y)
+
